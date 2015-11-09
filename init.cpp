f307ea4b1508a2afbca4706e297c6272d6f6453a
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2015-11-09 23:06:12 +0100

    work in progress to be compatible with simplified yothalot library

--- init.cpp
+++ init.cpp
@@ -14,42 +14,28 @@
 #include "job.h"
 #include "wrapper.h"
 #include "base.h"
+#include "stdin.h"
 #include <yothalot.h>
 
 /**
  *  Run the mapper
- *  @param  object          User supplied MapReduce object
  *  @param  input           All the input
+ *  @param  modulo          The modulo setting
  *  @return int
  */
-static int map(Php::Value &&object, Yothalot::Stdin &input)
+static int map(const Stdin &input, size_t modulo)
 {
     // prevent exceptions
     try
     {
         // wrap the php object
-        Wrapper mapreduce(std::move(object));
-
-        // prevent PHP output during the map/reduce algorithm
-        Php::call("ob_start");
-
-        // input data must be set
-        if (!input.data()) throw std::runtime_error("No input data is available");
-
-        // directory must be set
-        if (!input.directory()) throw std::runtime_error("No output directory is available");
+        Wrapper mapreduce(input.object());
 
         // create the task
-        Yothalot::MapTask task(base(), &mapreduce, input.modulo(), input.directory());
+        Yothalot::MapTask task(&mapreduce, modulo);
 
         // add the data to process
-        task.process(input.data(), strlen(input.data()));
-
-        // capture the output
-        std::string output = Php::call("ob_get_clean");
-
-        // did we have output?
-        if (output.size() > 0) throw std::runtime_error(output);
+        task.process(input.data(), input.size());
 
         // show output of mapper process
         std::cout << task.output();
@@ -69,35 +55,22 @@ static int map(Php::Value &&object, Yothalot::Stdin &input)
 
 /**
  *  Run the reducer
- *  @param  object          User supplied MapReduce object
  *  @param  input           All the input
  *  @return int
  */
-static int reduce(Php::Value &&object, Yothalot::Stdin &input)
+static int reduce(const Stdin &input)
 {
     // prevent exceptions
     try
     {
         // wrap php object
-        Wrapper mapreduce(std::move(object));
-
-        // prevent PHP output during the map/reduce algorithm
-        Php::call("ob_start");
-
-        // directory must be set
-        if (!input.directory()) throw std::runtime_error("No output directory is available");
+        Wrapper mapreduce(input.object());
 
         // create the task
-        Yothalot::ReduceTask task(base(), &mapreduce, input.index(), input.directory());
+        Yothalot::ReduceTask task(base(), &mapreduce);
 
-        // distribute the input files
-        input.distribute(&task);
-
-        // capture the output
-        std::string output = Php::call("ob_get_clean");
-
-        // did we have output?
-        if (output.size() > 0) throw std::runtime_error(output);
+        // add the data to process
+        task.process(input.data(), input.size());
 
         // show output of mapper process
         std::cout << task.output();
@@ -117,32 +90,22 @@ static int reduce(Php::Value &&object, Yothalot::Stdin &input)
 
 /**
  *  Run the writer/finalizer
- *  @param  object          User supplied MapReduce object
  *  @param  input           All the input
  *  @return int
  */
-static int write(Php::Value &&object, Yothalot::Stdin &input)
+static int write(const Stdin &input)
 {
     // prevent exceptions
     try
     {
         // wrap php object
-        Wrapper mapreduce(std::move(object));
-
-        // prevent PHP output during the map/reduce algorithm
-        Php::call("ob_start");
+        Wrapper mapreduce(input.object());
 
         // create the task
         Yothalot::WriteTask task(base(), &mapreduce);
 
-        // distribute the input files
-        input.distribute(&task);
-
-        // capture the output
-        std::string output = Php::call("ob_get_clean");
-
-        // did we have output?
-        if (output.size() > 0) throw std::runtime_error(output);
+        // add the data to process
+        task.process(input.data(), input.size());
 
         // show output of mapper process
         std::cout << task.output();
@@ -161,69 +124,27 @@ static int write(Php::Value &&object, Yothalot::Stdin &input)
 }
 
 /**
- *  Actually read the standard input
- *  @return std::string
- */
-static std::string readStdin()
-{
-    // see http://stackoverflow.com/questions/201992/how-to-read-until-eof-from-cin-in-c
-    // don't skip the whitespace while reading
-    std::cin >> std::noskipws;
-
-    // use stream iterators to copy the stream to a string
-    std::istream_iterator<char> it(std::cin);
-    std::istream_iterator<char> end;
-
-    // create the string and return it
-    return std::string(it, end);
-}
-
-/**
  *  Run a regular job (or a job that is part of a race, which is basically identical
  *  to a race job)
+ *  @param  input
  *  @return int
  */
-static int run()
+static int run(const Stdin &input)
 {
     // prevent exceptions
     try
     {
-        // read from stdin
-        auto stdin = readStdin();
-
-        // look for the \n\n separator
-        auto separator = stdin.find("\n\n");
-
-        // prevent PHP output during race algorithm
-        Php::call("ob_start");
-
-        // unserialize the first part of the stdin
-        auto unserialized = Php::call("unserialize", Php::call("base64_decode", stdin.substr(0, separator)));
-
-        // store the includes
-        auto includes = unserialized.get(0);
-
-        // loop over all our includes and include all of them once.
-        for (int i = 0; i < includes.size(); ++i)
-        {
-            // include the PHP file (this could cause a PHP fatal error)
-            if (!Php::include_once(includes.get(i).rawValue())) Php::error << "Failed to include " << includes.get(i) << std::flush;
-        }
-
-        // unserialize the inner object
-        auto object = Php::call("unserialize", unserialized.get(1));
-
         // call the process method
-        auto result = object.call("process", Php::call("unserialize", Php::call("base64_decode", stdin.substr(separator + 2))));
-
-        // if there's no output, the job generated no output
-        if (result.isNull()) return 0;
+        auto result = input.object().call("process", Php::call("unserialize", Php::call("base64_decode", Php::Value(input.data(), input.size()))));
 
         // capture the output
         std::string output = Php::call("ob_get_clean");
 
         // did we have output?
         if (output.size() > 0) throw std::runtime_error(output);
+
+        // if there's no output, the job generated no output
+        if (result.isNull()) return 0;
 
         // serialize the output, so that it can be unserialized at the caller side
         std::cout << Php::call("base64_encode", Php::call("serialize", result));
@@ -255,61 +176,29 @@ Php::Value yothalotInit(Php::Parameters &params)
     Php::call("ini_set", "error_log", nullptr); // disable the error_log
     Php::call("ini_set", "display_errors", "stderr");
 
-    // the run is the very fist task we may run
-    if (strcasecmp(params[0].rawValue(), "run") == 0) return run();
-
-    // read all data from input
-    Yothalot::Stdin input;
-
-    // parse the json
-    JSON::Object json(input.json());
-
-    // what files should we be including?
-    JSON::Array includes = json.array("includes");
-
-    // loop over all our includes and call require_once with all of them
-    for (int i = 0; i < includes.size(); ++i)
-    {
-        // required file must be a string
-        if (!includes.isString(i)) Php::error << "Include file is not a string" << std::flush;
-
-        // to prevent that the "require_once" statement echo's to stdout, we start output capturing
-        Php::call("ob_start");
-
-        // include the PHP file (this could cause a PHP fatal error)
-        if (!Php::include_once(includes.c_str(i))) Php::error << "Failed to include " << includes.c_str(i) << std::flush;
-
-        // capture the output
-        auto output = Php::call("ob_get_clean");
-
-        // we expect the output to be empty
-        if (output.size() == 0) continue;
-
-        // the PHP file is not supposed to create output, show this as error
-        Php::error << "Output was generated by included file " << includes.c_str(i) << " (" << output << ")" << std::flush;
-    }
-
-    // the "unserialize" algorithm could be written by the user, in which he accidentally left
-    // some output-generating code (which we dont want), capture this output
+    // prevent PHP output during race algorithm
     Php::call("ob_start");
+    
+    // read all input
+    Stdin input;
+    
+    // result variable
+    int result = -1;
+    
+    // the run is the very fist simple task
+    if (strcasecmp(params[0].rawValue(), "run") == 0) result = run(input);
 
-    // try to unserialize the original object
-    Php::Value unserialized = Php::call("unserialize", Php::call("base64_decode", json.c_str("object")));
+    // check the type of task to run that is part of the mapreduce algorithm
+    if (strcasecmp(params[0].rawValue(), "mapper")    == 0) result = map(input);
+    if (strcasecmp(params[0].rawValue(), "reducer")   == 0) result = reduce(input);
+    if (strcasecmp(params[0].rawValue(), "finalizer") == 0) result = write(input);
 
     // capture the output
     auto output = Php::call("ob_get_clean");
 
     // we expect the output to be empty
-    if (output.size() > 0) Php::error << "Output was generated when unserializing object (" << output << ")" << std::flush;
-
-    // check the type of task to run
-    if      (strcasecmp(params[0].rawValue(), "mapper")    == 0) return map(std::move(unserialized), input);
-    else if (strcasecmp(params[0].rawValue(), "reducer")   == 0) return reduce(std::move(unserialized), input);
-    else if (strcasecmp(params[0].rawValue(), "finalizer") == 0) return write(std::move(unserialized), input);
-
-    // program was started in unknown mode
-    Php::error << "Unrecognized input mode " << params[0] << std::flush;
+    if (output.size() > 0) Php::error << "Unexpected output (" << output << ")" << std::flush;
 
     // done with failure
-    return -1;
+    return result;
 }
---
+++ stdin.h
@@ -0,0 +1,111 @@
+/**
+ *  Stdin.h
+ *
+ *  Input that is read from stdin, and that contains the data for the
+ *  task, and the original serialized object
+ *
+ *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
+ *  @copyright 2015 Copernica BV
+ */
+
+/**
+ *  Class definition
+ */
+class Stdin
+{
+private:
+    /**
+     *  All the data
+     *  @var std::string
+     */
+    std::string _data;
+
+    /**
+     *  The unserialized PHP value
+     *  @var Php::Value
+     */
+    Php::Value _object;
+    
+    /**
+     *  The rest of the input data
+     *  @var const char *
+     */
+    const char *_rest;
+
+public:
+    /**
+     *  Constructor
+     */
+    Stdin()
+    {
+        // see http://stackoverflow.com/questions/201992/how-to-read-until-eof-from-cin-in-c
+        // don't skip the whitespace while reading
+        std::cin >> std::noskipws;
+
+        // use stream iterators to copy the stream to a string
+        std::istream_iterator<char> it(std::cin);
+        std::istream_iterator<char> end;
+
+        // create the string
+        _data.assign(it, end);
+        
+        // look for the \n\n separator
+        auto separator = _data.find("\n\n");
+        
+        // should exist
+        if (separator == std::string::npos) throw std::runtime_error("missing separator between serialized data and input data");
+        
+        // we now know where the rest of the data starts
+        _rest = _data.data() + separator + 2;
+
+        // unserialize the first part of the stdin
+        Php::Value unserialized(Php::call("unserialize", Php::call("base64_decode", Php::Value(_data.data(), separator))));
+
+        // store the includes
+        // @todo fix this
+        Php::Value includes(unserialized[0]);
+
+        // loop over all our includes and call require_once with all of them
+        for (int i = 0; i < includes.size(); ++i)
+        {
+            // include the PHP file (this could cause a PHP fatal error)
+            if (!Php::include_once(includes[i])) Php::error << "Failed to include " << includes[i] << std::flush;
+        }
+
+        // unserialize the inner object
+        _object = Php::call("unserialize", unserialized.get(1));
+    }
+    
+    /**
+     *  Destructor
+     */
+    virtual ~Stdin() = default;
+    
+    /**
+     *  The user-supplied PHP object
+     *  @return Php::Value
+     */
+    const Php::Value &object() const
+    {
+        return _object;
+    }
+    
+    /**
+     *  The input data
+     *  @return const char *
+     */
+    const char *data() const
+    {
+        return _rest;
+    }
+    
+    /**
+     *  Size of the data
+     *  @return size_t
+     */
+    size_t size() const
+    {
+        return _data.size() - (_rest - _data.data());
+    }
+};
+

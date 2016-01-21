59effb692028e25a991a83f1df047bfd33513c59
Toon Schoenmakers <toon.schoenmakers@copernica.com>
2016-01-21 11:07:29 +0100

    Added the ability to serialize connections

--- connection.h
+++ connection.h
@@ -24,7 +24,8 @@
  *  Class definition
  */
 class Connection :
-    public Php::Base {
+    public Php::Base,
+    public Php::Serializable {
 private:
     /**
      *  Shared pointer to the core
@@ -90,4 +91,55 @@ public:
      *  @return std::shared_ptr<Core>
      */
     const std::shared_ptr<Core> &core() const { return _core; }
+
+    /**
+     *  Method to serialize the object
+     *
+     *  This method should return a string representation of the object that
+     *  can be passed to the unserialize() method and that will revive the object
+     *
+     *  @return std::string
+     */
+    virtual std::string serialize() override
+    {
+        return _core->json();
+    }
+
+    /**
+     *  Unserialize the object
+     *
+     *  This method is called as an alternative __construct() method to initialize
+     *  the object. The passed in string parameter in in the format earlier returned
+     *  by a call to serialize()
+     *
+     *  @param  input           String to parse
+     *  @param  size            Size of the string
+     */
+    virtual void unserialize(const char *input, size_t size) override
+    {
+        // parse the json object
+        JSON::Object json(input, size);
+
+        // and extract all the optional parameters
+        std::string host = (json.contains("host") ? json.c_str("host") : "localhost");
+        std::string user = (json.contains("user") ? json.c_str("user") : "guest");
+        std::string password = (json.contains("password") ? json.c_str("password") : "guest");
+        std::string vhost = (json.contains("vhost") ? json.c_str("vhost") : "/");
+        std::string exchange = (json.contains("exchange") ? json.c_str("exchange") : "");
+        std::string mapreduce = (json.contains("mapreduce") ? json.c_str("mapreduce") : "mapreduce");
+        std::string races = (json.contains("races") ? json.c_str("races") : "races");
+        std::string jobs = (json.contains("jobs") ? json.c_str("jobs") : "jobs");
+
+        // creating a connection could throw
+        try
+        {
+            // create the actual connection
+            _core = std::make_shared<Core>(host, user, password, vhost, exchange, mapreduce, races, jobs);
+        }
+        catch (const std::runtime_error &error)
+        {
+            // convert C++ exception into a PHP exception
+            throw Php::Exception(error.what());
+        }
+    }
 };
